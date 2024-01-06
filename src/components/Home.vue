32495a89aa4b454574945168fb05168fae2d7fa2
Xin <78652542+him-li@users.noreply.github.com>
2024-01-06 23:31:09 +0200

    V0.1

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,31 +1,85 @@
 <template>
-  <v-container class="fill-height">
-    <v-responsive class="align-center text-center fill-height">
-      <v-textarea clearable label="Write a note" prepend-inner-icon="mdi-comment" variant="solo-filled"></v-textarea>
-
-      <v-btn variant="tonal">
-        Add Note
-      </v-btn>
-
-      <v-card class="mx-auto" max-width="344" hover>
-        <v-card-item>
-          <v-card-title>
-            Card title
-          </v-card-title>
-          <v-card-subtitle>
-            Card subtitle secondary text
-          </v-card-subtitle>
-        </v-card-item>
-        <v-card-text>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua.
-        </v-card-text>
-      </v-card>
-      <Note/>
-    </v-responsive>
-  </v-container>
+    <v-container class="fill-height">
+        <v-responsive class="align-center content-center fill-height">
+            <form @submit="handleSubmit(onSubmit)">
+                <v-text-field v-model="title.value" label="Title" />
+                <v-textarea clearable label="Write a note" prepend-inner-icon="mdi-comment" variant="solo-filled"
+                    v-model="content.value" />
+                <v-btn variant="tonal" type="submit">
+                    Add Note
+                </v-btn>
+                <v-btn variant="tonal" @click="resetForm">
+                    Clear
+                </v-btn>
+            </form>
+            <v-row class="notes">
+                <v-col v-for="(note, index) in notes" :key="index" cols="12" sm="6" md="4" lg="3">
+                    <v-card class="mx-auto" hover @click="openModal(index)">
+                        <v-card-item>
+                            <v-card-title>
+                                {{ note.title }}
+                            </v-card-title>
+                            <v-card-subtitle>
+                                {{ note.date }}
+                            </v-card-subtitle>
+                        </v-card-item>
+                        <v-card-text>
+                            {{ note.content }}
+                        </v-card-text>
+                    </v-card>
+                </v-col>
+            </v-row>
+            <Note :show="modal.show" :onHide="closeModal" :title="modal.title" :content="modal.content"
+                :date="modal.date" />
+        </v-responsive>
+    </v-container>
 </template>
 
 <script setup lang="ts">
-  import Note from './Note.vue'
+import { ref, reactive } from 'vue';
+import { useField, useForm } from 'vee-validate';
+import moment from 'moment';
+moment().format();
+import Note from './Note.vue';
+
+const { handleSubmit, resetForm } = useForm({
+    validationSchema: {
+        content: {
+            required: true,
+            message: 'Please write something.',
+        },
+    },
+});
+
+const title = reactive(useField('title'));
+const content = reactive(useField('content'));
+const notes: object = ref([]);
+const modal = reactive({
+    show: false,
+    title: '',
+    content: '',
+    date: '',
+});
+
+const onSubmit = async () => {
+    const newNote = {
+        title: title.value,
+        content: content.value,
+        date: moment().format('MMMM Do YYYY, h:mm:ss a'),
+    };
+    console.log(newNote)
+    notes.push(newNote);
+    resetForm();
+}
+
+const openModal = (index) => {
+    modal.title = notes.value[index].title;
+    modal.content = notes.value[index].content;
+    modal.date = notes.value[index].date;
+    modal.show = true;
+};
+
+const closeModal = () => {
+    modal.show = false;
+};
 </script>
--- src/components/Note.vue
+++ src/components/Note.vue
@@ -1,29 +1,28 @@
 <template>
-    <v-fade-transition hide-on-leave>
-        <v-card v-if="dialog" append-icon="$close" class="mx-auto" elevation="16" max-width="500" title="Send a receipt">
-            <template v-slot:append>
-                <v-btn icon="$close" variant="text" @click="dialog = false"></v-btn>
-            </template>
-
-            <v-divider></v-divider>
-
-            <div class="py-12 text-center">
-                <div>This receipt was sent</div>
-            </div>
-
-            <v-divider></v-divider>
-
-            <div class="pa-4 text-end">
-                <v-btn class="text-none" color="medium-emphasis" min-width="92" rounded variant="outlined"
-                    @click="dialog = false">
-                    Close
-                </v-btn>
-            </div>
+    <v-dialog :value="show" max-width="600" @input="onHide">
+        <v-card append-icon="$close" class="mx-auto" elevation="16" max-width="500">
+            <v-card-item>
+                <v-card-title class="text-h5">
+                    {{ date }}
+                </v-card-title>
+                <v-card-subtitle v-if="title" class="text-h6">
+                    {{ title }}
+                </v-card-subtitle>
+            </v-card-item>
+            <v-divider />
+            <v-card-text>
+                {{ text }}
+            </v-card-text>
+            <v-card-actions>
+                <v-btn class="content-none" color="medium-emphasis" min-width="92" rounded variant="outlined"
+                    @click="onHide">Close</v-btn>
+            </v-card-actions>
         </v-card>
-    </v-fade-transition>
+    </v-dialog>
 </template>
 
 <script setup lang="ts">
-    import { ref } from 'vue'
-    const dialog = ref(true)
+import { defineProps, ref } from 'vue';
+
+const props = defineProps(['show', 'onHide', 'title', 'text', 'date']);
 </script>
